c92eb9d27723762a32abc91dc8a186f2e1be3b30
Bartek Karkoszka <bkarkoszka@wp.pl>
2025-04-07 20:56:03 +0200

    fix: refactorized and fixed membership and progressoverwiew cards

--- src/pages/ClientDashboard/Membership.tsx
+++ src/pages/ClientDashboard/Membership.tsx
@@ -1,83 +1,45 @@
-import { Button, Flex, Progress, Typography } from 'antd';
-import { IdcardOutlined } from '@ant-design/icons';
+import { Button, Flex, Progress, Statistic, Typography, Col, Row, Space } from 'antd';
 import { Card } from '../../components/Card';
-
-const { Title } = Typography;
-
-// skonfiguruj sobie Prettiera żeby ci formatował np. przy zapisaniu pliku
-// to ci automatycznie będzie czytać config .prettierrc
-// i np. będzie robic wcięcia na 4 spacje
-// bo jak plik jest niesformatowany to mi go automatycznie sformatuje i później w historii edycji na gicie robi się syf
+import { CardTitle } from '../../components/CardTitle';
 
 export function Membership() {
     return (
-        // na te wszystkie panele dobrze zrobić komponent żeby paddingi i gapy były takie same
-        // ⬇️⬇️⬇️
         <Card>
-            {/* na taki tytuł z ikoną też dobrze zrobić komponent bo to będzie często używane */}
-            <Flex className='gap-small' align='center'>
-                {/* zrobiłem komponent Icon żeby ikonki były w miare spójne */}
-                {/* trzeba tylko dodać element w komponencie */}
-                {/* <Icon icon='membership' /> */}
-                <IdcardOutlined className='text-lg' />
-
-                {/* komponent z antd = mniej pisania klas */}
-                <Title level={4}>Membership</Title>
-            </Flex>
-
-            {/* grid ✅✅✅ */}
-            {/* ale znowu, w tym antd jest <Grid>...</Grid> to mniej pisania klas i czytelniej */}
-            <div className='grid grid-cols-2'>
-                {/* tutaj do tego jest gotowy komponent */}
-                {/* <Statistic title='Type' value='Standard' /> */}
-                <div className='flex flex-col justify-center'>
-                    {/* kolor staraj sie trzymać to co jest w mockupie */}
-                    {/* zamiast gray-*ileś* to jest normalnie text-neutral-5 */}
-                    {/* a jak brakuje to można dodać w głównym cssie */}
-                    <p className='text-sm text-gray-500'>Type</p>
-                    <p className='text-3xl'>Standard</p>
-                </div>
-
-                {/* <Flex>...</Flex> */}
-                <div className='justify-content flex items-center'>
-                    {/* to można do oddzielnego komponentu */}
-                    <Progress
-                        type='circle'
-                        percent={75}
-                        // ta funkcja renderuje normalnie komponent
-                        // wystarczy wrzucić jakiś div z tekstem i go poformatować
-                        // żeby był mniejszy i miał inny kolor
-                        format={() => '30 days left'}
-                        // też przy okazji patrze że można pomyśleć nad plikiem gdzie będą kolory w hexie
-                        // żeby sobie pobierać do kolorwania jakiś wykresów itd.
-                        strokeColor='#007bff'
-                        size={125}
-                    />
-                </div>
-
-                {/* <Flex>...</Flex> */}
-                <div className='col-5 flex flex-col pt-3'>
-                    <div>
-                        {/* <Statistic ... />*/}
-                        <p className='text-m text-gray-500'>Last Payment</p>
-                        <p className='text-xl font-semibold'>01.01.2025</p>
-
-                        {/* <Statistic ... />*/}
-                        <p className='text-m text-gray-500'>Valid Until</p>
-                        <p className='text-xl font-semibold'>20.02.2025</p>
-                    </div>
-                </div>
-                {/* <Space>...</Space> albo <Flex>...</Flex> */}
-                {/* i raczej gapy i paddingi z jakąś konkretną wartością to tak w ostateczności */}
-                {/* lepiej używać spacingu który jest w motywie bo później łatwo to zmienić w razie czego */}
-                {/* czyli gap-2 -> gap-small */}
-                <div className='col-5 flex items-center justify-end gap-2'>
-                    <Button type='primary' className='bg-blue-500'>
-                        Extend
-                    </Button>
-                    <Button>Show Details</Button>
-                </div>
-            </div>
+            <CardTitle title='Membership' icon='membership' />
+            <Row justify='space-between' className='w-'>
+                <Col>
+                    <Flex align='center' className='h-full'>
+                        <Statistic title='Type' value='Standard' />
+                    </Flex>
+                </Col>
+                <Col>
+                    <Flex vertical justify='center' className='h-full'>
+                        <Progress
+                            type='circle'
+                            percent={75}
+                            format={() => (
+                                <div className='text-font-secondary text-sm'>30 days left</div>
+                            )}
+                            strokeColor='#007bff'
+                            size={125}
+                        />
+                    </Flex>
+                </Col>
+                <Col>
+                    <Space direction='vertical' size={'middle'}>
+                        <div>
+                            <Statistic title='Last Payment' value='01.01.2025' />
+                            <Statistic title='Valid Until' value='20.02.2025' />
+                        </div>
+                        <div>
+                            <Button type='primary' className='bg-blue-500'>
+                                Extend
+                            </Button>
+                            <Button>Show Details</Button>
+                        </div>
+                    </Space>
+                </Col>
+            </Row>
         </Card>
     );
 }
--- src/pages/ClientDashboard/ProgressOverview.tsx
+++ src/pages/ClientDashboard/ProgressOverview.tsx
@@ -1,60 +1,27 @@
-import { RiseOutlined } from '@ant-design/icons';
-
-// przeczytaj najpierw plik z membershipem
-// bo tu podobne wskazówki
+import { Card } from '../../components/Card';
+import { Row, Statistic, Typography } from 'antd';
+import { CardTitle } from '../../components/CardTitle';
 
 export function ProgressOverview() {
     return (
-        // <Card>...</Card>
-        // height na sztywno ❌
-        <div className='bg-card flex h-44 w-1/2 flex-col p-5 shadow-md'>
-            {/* <Flex>...</Flex> */}
-            <div className='flex items-center gap-2'>
-                {/* text-blue-500 -> text-primary */}
-                <RiseOutlined className='text-lg text-blue-500' />
-                {/* <Title>...</Title> */}
-                <h2 className='text-lg font-semibold'>Progress Overview</h2>
-            </div>
+        <Card>
+            <CardTitle title='Progress Overview' icon='progress' />
 
-            {/* ten <Statistic> w biblotece nie ma tych trendów bo chyba usuneli */}
-            {/* to można zrobić komponent oddzielny żeby to miał */}
-            <div className='grid grid-cols-2 gap-y-1'>
-                {/* <Statistic ... /> */}
-                <div>
-                    <p className='text-xs text-gray-500'>Weekly Total Sets</p>
-                    <p className='mb-1 text-2xl font-bold'>86</p>
-                </div>
-                {/* <Statistic ... /> */}
-                <div>
-                    <p className='text-xs text-gray-500'>Weekly Session Volume</p>
-                    <p className='mb-1 text-2xl font-bold'>32,893 kg</p>
-                </div>
-            </div>
-
-            {/* <Grid>...</Grid> */}
-            {/* dużo niepotrzebnych gapów chyba */}
-            {/* te trendy też mogą być oddzielnym komponentem */}
-            {/* i wtedy ten statistic nowy to będzie po prostu <Statistic /> i ten komponent (np. <Trend />) */}
-            <div className='mt-auto grid grid-cols-2 gap-y-1'>
-                {/* <Flex>...</Flex> */}
-                <div className='flex items-center gap-1'>
-                    <p className='text-xs text-gray-500'>since last week</p>
-                    <p className='flex items-center text-xs font-bold text-green-500'>
-                        {/* staraj sie zawsze owijać tekst w jakiś znacznik to później łatwo go formatować */}
-                        {/* np. <span>10%</span> */}
-                        {/* <Icon icon=... /> */}
-                        <RiseOutlined className='mr-0.5' /> 10%
-                    </p>
-                </div>
-                {/* <Flex>...</Flex>, text-gray -> text-neutral-..., text-green -> text-success */}
-                <div className='flex items-center gap-1'>
-                    <p className='text-xs text-gray-500'>since last week</p>
-                    <p className='flex items-center text-xs font-bold text-green-500'>
-                        {/* <Icon icon=... /> */}
-                        <RiseOutlined className='mr-0.5' /> 12.5%
-                    </p>
-                </div>
-            </div>
-        </div>
+            <Row justify={'space-between'} className='w-full'>
+                <Statistic title='Weekly Total Sets' value={86} />
+                <Statistic title='Weekly Session Volume' value='32,893 kg' />
+            </Row>
+            <Row justify={'space-between'} className='w-full'>
+                <Row className='gap-3'>
+                    <p className='text-neutral-6 text-md'>since last week</p>
+                    <p className='text-md flex items-center text-green-400'>10%</p>
+                </Row>
+                {/*nie wiedzialem jak wyrownac te Trendy dlatego takie gapy dziwne*/}
+                <Row className='gap-3.5'>
+                    <p className='text-neutral-6 text-md'>since last week</p>
+                    <p className='text-md flex items-center text-green-400'>12.5%</p>
+                </Row>
+            </Row>
+        </Card>
     );
 }
