60a19574c616b001aabae1765d7834f94859f851
rafal <you@example.com>
2025-04-24 15:35:55 +0200

    fix: migrate flake to new devshell scehma

--- flake.nix
+++ flake.nix
@@ -2,17 +2,23 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  outputs =
     {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs_22
-        ];
-      };
-    }
-  );
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+        };
+      }
+    );
 }
-
