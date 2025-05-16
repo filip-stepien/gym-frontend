a8054e883c3b7bad5e7922a415757303806dc50c
Filip Stępień <filip.stepien30@gmail.com>
2025-05-16 21:34:49 +0200

    feat: use fullscreen schedule viewer in sessions

--- src/pages/ClientSession/ClientSession.tsx
+++ src/pages/ClientSession/ClientSession.tsx
@@ -1,12 +1,40 @@
-import { Flex } from 'antd';
-import { CalendarCard } from './CalendarCard';
-import { YourUpcomingSession } from './YourUpcomingSessions';
+import { Card } from '@/components/Card';
+import { CardTitle } from '@/components/CardTitle';
+import { ScheduleViewer } from '@/components/ScheduleViewer';
+import { ScheduleDateListElement } from '@/components/ScheduleViewer/ScheduleViewer';
+import { Button } from 'antd';
+import { Avatar } from '@/components/Avatar';
+import dayjs from 'dayjs';
+
+const listElements: ScheduleDateListElement[] = [
+    {
+        date: dayjs('2025-06-16'),
+        title: 'Workout 1',
+        description: 'description',
+        avatar: <Avatar />,
+        action: <Button>Join</Button>
+    },
+    {
+        date: dayjs('2025-06-15'),
+        title: 'Workout 1',
+        description: 'description',
+        avatar: <Avatar />,
+        action: <Button>Join</Button>
+    },
+    {
+        date: dayjs('2025-06-15'),
+        title: 'Workout 1',
+        description: 'description',
+        avatar: <Avatar />,
+        action: <Button>Join</Button>
+    }
+];
 
 export function ClientSession() {
     return (
-        <Flex vertical className='gap-layout'>
-            <CalendarCard />
-            <YourUpcomingSession />
-        </Flex>
+        <Card>
+            <CardTitle title='Your sessions' icon='sessions' />
+            <ScheduleViewer listElements={listElements} fullscreen />
+        </Card>
     );
 }
