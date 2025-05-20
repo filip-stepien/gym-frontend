5a6e365e4b2e9634bbe5b6a59a3804a146431fc7
Filip Stępień <filip.stepien30@gmail.com>
2025-05-20 15:17:16 +0200

    feat: coach training sessions

--- src/pages/CoachSessions/CoachSessions.tsx
+++ src/pages/CoachSessions/CoachSessions.tsx
@@ -1,3 +1,36 @@
+import { Card } from '@/components/Card';
+import { CardTitle } from '@/components/CardTitle';
+import { ScheduleViewer } from '@/components/ScheduleViewer';
+import { ScheduleDateListElement } from '@/components/ScheduleViewer/ScheduleViewer';
+import { Button } from 'antd';
+import dayjs from 'dayjs';
+
+const scheduleListElements: ScheduleDateListElement[] = [
+    {
+        date: dayjs(),
+        description: 'workout description',
+        title: 'Workout 1',
+        action: <Button>Details</Button>
+    },
+    {
+        date: dayjs(),
+        description: 'workout description',
+        title: 'Workout 2',
+        action: <Button>Details</Button>
+    }
+];
+
+const scheduleActions = [<Button type='primary'>+ New Session</Button>];
+
 export function CoachSessions() {
-    return <div></div>;
+    return (
+        <Card>
+            <CardTitle title='Training Sessions' icon='sessions' />
+            <ScheduleViewer
+                fullscreen
+                listElements={scheduleListElements}
+                actions={scheduleActions}
+            />
+        </Card>
+    );
 }
