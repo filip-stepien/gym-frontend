22a7186a22ed6ee27bed0c19a8b83d37d8c95954
rafal <you@example.com>
2025-05-30 12:12:58 +0200

    chore: remove orphans

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { Router } from './router';
 import { Provider } from './Provider';
-import { useEffect } from 'react';
-import keycloak from './keycloak';
-import { useLocation } from 'react-router';
 
 export function App() {
     return (
