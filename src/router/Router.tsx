ad2d432132fbb4dc40ef88f64f72cb5f755be01d
Filip Stępień <filip.stepien30@gmail.com>
2025-06-02 18:44:16 +0200

    Revert "fix: client details page in router"
    This reverts commit ac4c19a4130d04543fe5f5b71e0b09c329545688.

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -30,7 +30,6 @@ import { ManagerTrainingHalls } from '@/pages/ManagerTrainingHalls';
 import { ManagerHallDetails } from '@/pages/ManagerHallDetails';
 import { ManagerNewHall } from '@/pages/ManagerNewHall';
 import { EmployeeHallDetails } from '@/pages/EmployeeHallsDetails';
-import { EmployeeClientsDetails } from '@/pages/EmployeeClientsDetails';
 export function Router() {
     return (
         <BrowserRouter>
@@ -57,7 +56,7 @@ export function Router() {
                         <Route path='training-halls' element={<EmployeeTrainingHalls />} />
                         <Route path='notifications' element={<EmployeeNotifications />} />
                         <Route path='create-membership' element={<EmployeeClientsCreation />} />
-                        <Route path='client-details' element={<EmployeeClientsDetails />} />
+                        <Route path='client-details' element={<EmployeeHallDetails />} />
                         <Route path='training-halls/:id' element={<EmployeeHallDetails />} />
                     </Route>
 
