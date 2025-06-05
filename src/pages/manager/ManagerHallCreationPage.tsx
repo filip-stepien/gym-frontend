586652b03b03cffb0f0c15a368861ed6cfa42982
rafal <you@example.com>
2025-06-05 20:10:07 +0200

    feat: createHallPage fetch hallTypes from api

--- src/pages/manager/ManagerHallCreationPage.tsx
+++ src/pages/manager/ManagerHallCreationPage.tsx
@@ -1,15 +1,32 @@
 import { BackButton } from '@/components/layout/BackButton';
 import { HallCreationCard, HallValues } from '@/components/cards/HallCreationCard';
-
-const hallCreationCardData = {
-    hallTypes: ['Yoga', 'Weight lifting', 'Cardio'],
-    onCreate: (values: HallValues) => {
-        // POST...
-        console.log(values);
-    }
-};
+import { listHallTypes } from '@/generated/gym-api';
+import { useEffect, useState } from 'react';
 
 export function ManagerHallCreationPage() {
+    const [hallTypes, setHallTypes] = useState<string[]>([]);
+
+    const hallCreationCardData = {
+        hallTypes,
+        onCreate: (values: HallValues) => {
+            // POST...
+            console.log(values);
+        }
+    };
+
+    useEffect(() => {
+        async function getData() {
+            const hallTypesData = (await listHallTypes()).data
+                .filter(hall => hall !== undefined)
+                .map(hall => hall.name)
+                .filter(name => name !== undefined);
+
+            setHallTypes(hallTypesData);
+        }
+
+        getData();
+    }, []);
+
     return (
         <div>
             <BackButton />
