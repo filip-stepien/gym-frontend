336d8525459b51f13a35965bef502ee999907ebb
rafal <you@example.com>
2025-06-07 20:41:16 +0200

    feat: progress overview card api connection

--- src/components/cards/ProgressOverviewCard.tsx
+++ src/components/cards/ProgressOverviewCard.tsx
@@ -1,6 +1,10 @@
 import { Card } from '@/components/layout/Card';
 import { CardTitle } from '@/components/common/CardTitle';
 import { TrendStatistic } from '@/components/common/TrendStatistic';
+import { useEffect, useState } from 'react';
+import { getUserProgressOverview, ProgressOverviewDto } from '@/generated/gym-api';
+import { useUser } from '@/hooks/useUser';
+import { DataStateWrapper } from '../common/DataStateWrapper';
 
 type ProgressOverviewCardProps = {
     weeklyTotalSets?: {
@@ -23,29 +27,53 @@ function formatTrendString(trend?: number) {
 }
 
 export function ProgressOverviewCard(props: ProgressOverviewCardProps) {
-    const { weeklyTotalSets, weeklySessionVolume } = props;
+    const [stats, setStats] = useState<ProgressOverviewDto>();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const userId = useUser().user?.id;
+    // const { weeklyTotalSets, weeklySessionVolume } = props;
+
+    useEffect(() => {
+        const loadStats = async () => {
+            if (!userId) return;
+
+            try {
+                setIsLoading(true);
+                setStats((await getUserProgressOverview(userId)).data);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        loadStats();
+    }, [userId]);
+
+    const showEmpty = !userId || !stats;
+
     return (
         <Card className='flex-1'>
             <CardTitle title='Progress Overview' icon='progress' />
             <div className='gap-large flex w-full flex-col sm:flex-row'>
-                <TrendStatistic
-                    title='Weekly Total Sets'
-                    value={weeklyTotalSets?.value ?? '-'}
-                    trend={{
-                        title: 'since last week',
-                        value: formatTrendString(weeklyTotalSets?.trend),
-                        direction: getTrendDirection(weeklyTotalSets?.trend)
-                    }}
-                />
-                <TrendStatistic
-                    title='Weekly Session Volume'
-                    value={weeklySessionVolume?.value ?? '-'}
-                    trend={{
-                        title: 'since last week',
-                        value: formatTrendString(weeklySessionVolume?.trend),
-                        direction: getTrendDirection(weeklySessionVolume?.trend)
-                    }}
-                />
+                <DataStateWrapper isEmpty={showEmpty} isLoading={isLoading}>
+                    <TrendStatistic
+                        title='Weekly Total Sets'
+                        value={stats?.weeklyTotalSets?.value ?? '-'}
+                        trend={{
+                            title: 'since last week',
+                            value: formatTrendString(stats?.weeklyTotalSets?.trend),
+                            direction: getTrendDirection(stats?.weeklyTotalSets?.trend)
+                        }}
+                    />
+                    <TrendStatistic
+                        title='Weekly Session Volume'
+                        value={stats?.weeklySessionVolume?.value ?? '-'}
+                        trend={{
+                            title: 'since last week',
+                            value: formatTrendString(stats?.weeklySessionVolume?.trend),
+                            direction: getTrendDirection(stats?.weeklySessionVolume?.trend)
+                        }}
+                    />
+                </DataStateWrapper>
             </div>
         </Card>
     );
