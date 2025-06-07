79de4311a7497eabfe37b8047e931e5258d256d0
rafal <you@example.com>
2025-06-07 19:14:15 +0200

    feat: provide userId to membershipStatusCard componente of clientMembershipPage

--- src/pages/client/ClientMembershipPage.tsx
+++ src/pages/client/ClientMembershipPage.tsx
@@ -2,6 +2,7 @@ import { Page } from '@/components/layout/Page';
 import { PersonalDetailsCard } from '@/components/cards/PersonalDetailsCard';
 import { MembershipStatusCard } from '@/components/cards/MembershipStatusCard';
 import dayjs from 'dayjs';
+import { useUser } from '@/hooks/useUser';
 
 const membershipOverviewCardData = {
     firstName: 'John',
@@ -16,10 +17,11 @@ const membershipStatusData = {
 };
 
 export function ClientMembershipPage() {
+    const userId = useUser().user?.id;
     return (
         <Page>
             <PersonalDetailsCard {...membershipOverviewCardData} />
-            <MembershipStatusCard {...membershipStatusData} />
+            <MembershipStatusCard {...membershipStatusData} userId={userId} />
         </Page>
     );
 }
