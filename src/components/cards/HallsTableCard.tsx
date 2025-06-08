268705613d4a8c5b3c17bddc9ef87df6ad94491a
rafal <you@example.com>
2025-06-08 21:37:10 +0200

    feat: hall table pagination :spaghetti:

--- src/components/cards/HallsTableCard.tsx
+++ src/components/cards/HallsTableCard.tsx
@@ -2,6 +2,8 @@ import { CardTitle } from '@/components/common/CardTitle';
 import { Card, Table, TableColumnsType, Tag, Badge, Flex } from 'antd';
 import { Link } from 'react-router';
 import { ActionButton } from '../common/ActionButton';
+import { useEffect, useState } from 'react';
+import { HallDto, listHalls } from '@/generated/gym-api';
 
 export type HallsTableHall = {
     hallNumber: string;
@@ -11,8 +13,9 @@ export type HallsTableHall = {
 };
 
 type HallsTableCardProps = {
-    halls?: HallsTableHall[];
+    // halls?: HallsTableHall[];
     newHallHref?: string;
+    defaultPageSize?: number;
 };
 
 type DataType = {
@@ -22,24 +25,24 @@ type DataType = {
 const columns: TableColumnsType<DataType> = [
     {
         title: 'Hall number',
-        dataIndex: 'hallNumber',
+        dataIndex: 'hallName',
         key: 'hallNumber',
         fixed: 'left',
-        sorter: (a, b) => a.hallNumber.localeCompare(b.hallNumber)
+        sorter: true
     },
     {
         title: 'Hall type',
         dataIndex: 'hallType',
         key: 'hallType',
         fixed: 'left',
-        sorter: (a, b) => a.hallType.localeCompare(b.hallType),
+        sorter: true,
         render: (halltype: string) => <Tag>{halltype}</Tag>
     },
     {
         title: 'Status',
         dataIndex: 'hallStatus',
         key: 'hallStatus',
-        sorter: (a, b) => a.hallStatus.localeCompare(b.hallStatus),
+        sorter: false,
         render: (status: string) => {
             let badgeStatus: 'success' | 'warning' | 'error' | 'default';
 
@@ -70,7 +73,73 @@ const columns: TableColumnsType<DataType> = [
     }
 ];
 
-export function HallsTableCard({ halls = [], newHallHref }: HallsTableCardProps) {
+export function HallsTableCard({ defaultPageSize, newHallHref }: HallsTableCardProps) {
+    const [sorter, setSorter] = useState<{
+        field: string | undefined;
+        order: 'ascend' | 'descend' | undefined;
+    }>();
+    const [loading, setLoading] = useState(false);
+    const [halls, setHalls] = useState<HallsTableHall[] | undefined>([]);
+    const [pagination, setPagination] = useState<{ page: number; pageSize: number } | undefined>();
+    const [totalElements, setTotalElements] = useState<number | undefined>(undefined);
+
+    useEffect(() => {
+        const getData = async (
+            pageNumber: number,
+            pageSize?: number,
+            sortField?: string,
+            sortOrder?: 'descend' | 'ascend'
+        ): Promise<{ data: HallsTableHall[]; total: number } | undefined> => {
+            try {
+                const response = await listHalls({
+                    // api counts from 0 , frontend from 1
+                    page: pageNumber - 1,
+                    size: pageSize,
+                    // bug workaround
+                    sort: `${sortField ?? ''},${sortOrder === 'descend' ? 'desc' : 'asc'}` as unknown as string[]
+                });
+                const rawHalls = response?.data?.content;
+                const totalElements = response?.data?.totalElements;
+
+                if (!rawHalls || !totalElements) {
+                    return undefined;
+                }
+                console.log(rawHalls);
+
+                const transformedHalls: HallsTableHall[] = rawHalls.map((hall: HallDto) => ({
+                    ...hall,
+                    hallNumber: hall.hallName ?? 'None',
+                    hallType: hall.hallType?.name ?? 'None',
+                    hallStatus: 'Available',
+                    detailsHref: `details/${hall.uuid ?? ''}`
+                }));
+
+                return {
+                    data: transformedHalls,
+                    total: totalElements
+                };
+            } catch (error) {
+                console.error('Błąd podczas pobierania hal:', error);
+                return undefined;
+            }
+        };
+
+        console.log(sorter, pagination);
+        setLoading(true);
+
+        getData(
+            pagination?.page ?? 1,
+            pagination?.pageSize,
+            sorter?.field ?? '',
+            sorter?.order ?? 'ascend'
+        )
+            .then(data => {
+                setHalls(data?.data);
+                setTotalElements(data?.total);
+            })
+            .finally(() => setLoading(false));
+    }, [sorter, pagination]);
+
     return (
         <Card className='w-full'>
             <Flex vertical className='gap-layout'>
@@ -83,9 +152,31 @@ export function HallsTableCard({ halls = [], newHallHref }: HallsTableCardProps)
                     )}
                 </Flex>
                 <Table<DataType>
-                    pagination={false}
+                    pagination={{
+                        pageSizeOptions: [2, 5, 10, 20, 50, 100],
+                        defaultPageSize: defaultPageSize,
+                        pageSize: pagination?.pageSize,
+                        total: totalElements,
+                        showSizeChanger: true
+                    }}
+                    loading={loading}
+                    onChange={(pagination, _filter, newSorter) => {
+                        if (pagination.current && pagination.pageSize) {
+                            setPagination({
+                                page: pagination.current,
+                                pageSize: pagination.pageSize
+                            });
+                        } else {
+                            console.warn('something went wrong');
+                        }
+                        const singleSorter = Array.isArray(newSorter) ? newSorter[0] : newSorter;
+                        setSorter({
+                            field: singleSorter.field?.toString() ?? undefined,
+                            order: singleSorter.order ?? undefined
+                        });
+                    }}
                     columns={columns}
-                    dataSource={halls.map((col, i) => ({ key: i, ...col }))}
+                    dataSource={halls?.map((col, i) => ({ key: i, ...col }))}
                     scroll={{ x: 'max-content' }}
                 />
             </Flex>
--- src/pages/common/HallsPage.tsx
+++ src/pages/common/HallsPage.tsx
@@ -1,36 +1,11 @@
 import { HallsTableCard, HallsTableHall } from '@/components/cards/HallsTableCard';
 import { HallDto, listHalls } from '@/generated/gym-api';
 import { getRoleFromUrl } from '@/utils/getRoleFromUrl';
+import { i } from 'node_modules/react-router/dist/development/lib-B8x_tOvL.d.mts';
 import { useEffect, useState } from 'react';
 
 const role = getRoleFromUrl();
 
 export function HallsPage() {
-    const [halls, setHalls] = useState<HallsTableHall[]>([]);
-
-    useEffect(() => {
-        async function getData() {
-            try {
-                const response = await listHalls();
-                const rawHalls = response.data.content ?? [];
-
-                const transformedHalls: HallsTableHall[] = rawHalls.map((hall: HallDto) => ({
-                    ...hall,
-                    hallNumber: hall.hallName ?? 'None',
-                    hallType: hall.hallType?.name ?? 'None',
-                    hallStatus: 'Available',
-                    detailsHref: `details/${hall.uuid ?? ''}`
-                }));
-
-                setHalls(transformedHalls);
-            } catch (error) {
-                console.error('Błąd podczas pobierania hal:', error);
-                setHalls([]);
-            }
-        }
-
-        getData();
-    }, []);
-
-    return <HallsTableCard halls={halls} newHallHref={`/${role}/create-hall`} />;
+    return <HallsTableCard newHallHref={`/${role}/create-hall`} />;
 }
