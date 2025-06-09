d8b34a3f8f40735827c8eecd47c4fc8ac2749075
Bartek Karkoszka <bkarkoszka@wp.pl>
2025-06-09 02:13:09 +0200

    feat: link employee table to API

--- src/pages/manager/ManagerEmployeesPage.tsx
+++ src/pages/manager/ManagerEmployeesPage.tsx
@@ -1,29 +1,40 @@
 import { EmployeesTableCard } from '@/components/cards/EmployeesTableCard';
+import { UserDto, listUsers } from '@/generated/gym-api';
+import { useState, useEffect } from 'react';
+import { getRoleFromUrl } from '@/utils/getRoleFromUrl';
 
-const employeesTableCardData = {
-    newEmployeeHref: '/manager/create-employee',
-    employees: [
-        {
-            firstName: 'John',
-            lastName: 'Pork',
-            position: 'Coach',
-            detailsHref: '/manager/employees/1'
-        },
-        {
-            firstName: 'Bob',
-            lastName: 'Beef',
-            position: 'Employee',
-            detailsHref: '/manager/employees/1'
-        },
-        {
-            firstName: 'Tim',
-            lastName: 'Cheese',
-            position: 'Employee',
-            detailsHref: '/manager/employees/1'
-        }
-    ]
-};
+const role = getRoleFromUrl();
 
 export function ManagerEmployeesPage() {
+    const [users, setUsers] = useState<UserDto[]>([]);
+
+    useEffect(() => {
+        async function getClients() {
+            try {
+                const response = await listUsers();
+                const rawClients = response.data?.content ?? [];
+
+                const transformedClients: UserDto[] = rawClients.map((user: UserDto) => ({
+                    firstName: user.firstName ?? 'Unknown',
+                    lastName: user.lastName ?? 'Unknown',
+                    position: user.roles ?? 'Unknown',
+                    detailsHref: `/${role}/details/${user.uuid ?? ''}`
+                }));
+
+                setUsers(transformedClients);
+            } catch (error) {
+                console.error('Błąd podczas pobierania użytkowników:', error);
+                setUsers([]);
+            }
+        }
+
+        getClients();
+    }, [role]);
+
+    const employeesTableCardData = {
+        employees: users,
+        newEmployeeHref: ['manager'].includes(role) ? `/${role}/create-employee` : undefined
+    };
+
     return <EmployeesTableCard {...employeesTableCardData} />;
 }
