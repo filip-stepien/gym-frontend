6b0072893e49debc9fec2e9b5152166880c1e270
konicynke <koniczynka121@interia.eu>
2025-06-10 16:22:26 +0200

    feat: integrate newssesioncardwithdata in coachnewsessionpage

--- src/pages/coach/CoachNewSessionPage.tsx
+++ src/pages/coach/CoachNewSessionPage.tsx
@@ -1,14 +1,5 @@
-import { NewSessionCard, type NewSessionValues } from '@/components/cards/NewSessionCard';
-
-const newSessionCardData = {
-    exerciseSearchOptions: ['Bench press', 'Pull up', 'Lateral raise'],
-    trainingHallNumbers: ['1', '2', '3', '4'],
-    onCreate: (values: NewSessionValues) => {
-        // POST...
-        console.log(values);
-    }
-};
+import { NewSessionCardWithData } from '@/components/cards/with-data/NewSessionCardWithData';
 
 export function CoachNewSessionPage() {
-    return <NewSessionCard {...newSessionCardData} />;
+    return <NewSessionCardWithData />;
 }
