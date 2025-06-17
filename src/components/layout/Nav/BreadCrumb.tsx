343ffdddb3d70af093276889c24b8946b7c1340f
Filip Stępień <filip.stepien30@gmail.com>
2025-06-17 10:58:35 +0200

    feat: hide uuids from breadcrumb

--- src/components/layout/Nav/BreadCrumb.tsx
+++ src/components/layout/Nav/BreadCrumb.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumb as AntBreadCrumb } from 'antd';
 import { useLocation } from 'react-router';
+import validator from 'validator';
 
 type BreadCrumbProps = {
     className?: string;
@@ -14,9 +15,9 @@ export function BreadCrumb({ className }: BreadCrumbProps) {
     const breadCrumbItems = location.pathname
         .split('/') // tokenize path
         .filter(path => path) // remove empty strings
+        .map(pathname => (validator.isUUID(pathname) ? 'Details' : pathname)) // hide uuids
         .map(pathname => pathname.charAt(0).toUpperCase() + pathname.slice(1)) // capitalize pathnames
         .map(pathname => ({ title: pathname })); // convert to breadcrumb items
-
     return (
         <AntBreadCrumb className={className} items={[firstBreadCrumbItem, ...breadCrumbItems]} />
     );
